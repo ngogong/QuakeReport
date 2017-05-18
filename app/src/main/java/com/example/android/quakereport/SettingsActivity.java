54ccca50665f18b7021cfe3280884e095f789dd7
ngogonggongngo.0000001@gmail.com <(:abcde12345:)>
2017-05-17 19:37:22 -0700

    after ud843-lesson5-after-step3

---
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.quakereport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ismile on 5/17/2017.
+ */
+
+public class SettingsActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.settings_activity);
+        }
+
+        public static class EarthquakePreferenceFragment extends PreferenceFragment {
+
+        }
+}
+
